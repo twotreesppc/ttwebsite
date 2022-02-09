19b253c4f1889b94af2bd3cd84355c32af1a6314
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-08 17:51:39 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
             path: `${__dirname}/src/images`,
         },
         },
-        `gatsby-plugin-image`
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {

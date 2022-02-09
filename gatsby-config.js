5939589363fb7aae3713120b4658b85d752c9627
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-08 17:48:18 -0800

    image

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
             path: `${__dirname}/src/images`,
         },
         },
+        `gatsby-plugin-image`
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
@@ -27,6 +28,7 @@ module.exports = {
             icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
         },
         },
+        
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,

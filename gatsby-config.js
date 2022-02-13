97a998926ed124190c8fc585e03e3802072ebb92
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-13 13:29:25 -0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,11 @@ module.exports = {
         {
             resolve: `gatsby-source-wordpress`,
             options: {
+                production: {
+                
+            allow404Images: true
+            },
+        
               url:
               // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
                 process.env.WPGRAPHQL_URL ||

6d401db5e43908b53dd436a68b30486a749a8901
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-16 10:29:25 -0800

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
             options: {
               spaceId: process.env.CONTENTFUL_SPACE_ID,
               accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-              host: `preview.contentful.com`,
             },
           },
         `gatsby-plugin-react-helmet`,

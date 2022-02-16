7f9fbb98b9ee2de8f7693e7a30a2468f091271cc
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-16 10:24:55 -0800

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
             resolve: `gatsby-source-contentful`,
             options: {
               spaceId: process.env.CONTENTFUL_SPACE_ID,
-              accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
               host: `preview.contentful.com`,
             },
           },

4b83ddaf603071e577fe4aa408f1bb780a9fb9d5
twotreesppc <98858692+twotreesppc@users.noreply.github.com>
2022-02-16 10:33:37 -0800

    newcode

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
         author: `Two Trees PPC`,
     },
     plugins: [
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+              spaceId: process.env.CONTENTFUL_SPACE_ID,
+              accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+          },
         `gatsby-plugin-react-helmet`,
         {
         resolve: `gatsby-source-filesystem`,
